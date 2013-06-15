28d9a774d2086405569597edee91abc6dfa22194
Baptiste Langlade <baptouuuu@gmail.com>
2013-06-15 12:56:23 +0200

    fixed interface syntax

--- src/HE/Html/Element/ElementInterface.php
+++ src/HE/Html/Element/ElementInterface.php
@@ -4,9 +4,9 @@ namespace HE\Html\Element;
 
 interface ElementInterface {
 
-    public function setAttribute($name, $data) {};
-    public function getAttribute($name) {};
-    public function render() {};
-    public function __toString() {};
+    public function setAttribute($name, $data);
+    public function getAttribute($name);
+    public function render();
+    public function __toString();
 
 }
